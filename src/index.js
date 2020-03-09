ba624f545b32324e7bcdc29b6ff9fbb630ff5382
Artem <artem-git@clinicbuddy.com>
2020-03-09 15:34:54 +0300

    feat: add solved Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var i = 0,
+      bracketSample;
+
+  while (i < bracketsConfig.length) {
+    bracketSample = bracketsConfig[i].join('');
+
+    if (!str.includes(bracketSample)) {
+      i++;
+    } else {
+      str = str.replace(bracketSample,'');
+      i = 0;
+    }
+  }
+
+  if (str.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
